80f12472ad39a8874732409f9d3d3f530663b437
finn42 <finnlattimore@gmail.com>
2015-11-07 15:53:58 +1100

    fixes indicator test so LEDs come on in board order

--- code/indicatorLEDTest_PCB/indicatorLEDTest_PCB.ino
+++ code/indicatorLEDTest_PCB/indicatorLEDTest_PCB.ino
@@ -1,5 +1,11 @@
+// This script tests the RED indicator LEDs.
+// If all is going well, they should come on in order for half a second each.
+// If any of the LEDs do not come on, check the soldering/connections on your PCB (use a multimeter on the connectivity/beep setting)
+
+
 int indx = 0;
-int redLeds[] = {18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33};
+int redLeds[] = {30,26,22,18,19,23,27,31,32,28,24,20,33,29,25,21};
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   for (int i = 0; i < 16; i++){
