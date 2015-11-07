74b6888c39017b32a82dedff17cb26efca637542
finn42 <finnlattimore@gmail.com>
2015-11-07 15:45:40 +1100

    tmp

--- code/readdata.py
+++ code/readdata.py
@@ -1,22 +1,31 @@
 import serial
 
-N = 100
-FILE_NAME = "board.txt"
+COLORS = ["blue","red","green","yellow"]
+PORT = "/dev/ttyUSB0"
+BAUDRATE = 9600
+SAMPLES_PER_POSITION = 10
 
+def configurations(color_list):
+    l = range(len(color_list))
+    result = []
+    for i in l:
+        order = [(x - i)%len(color_list) for x in l]
+        colors = [color_list[i] for i in order]
+        result.append(colors)
+    return result
+        
 
-o = open(FILE_NAME,"w")
-
-for config in range(4):
-    raw_input("Please set board to configuration {}. Press any key to continue".format(config+1))
+configs = configurations(colors)
+for config in configs:
+    raw_input("Please set board to configuration {}. Press any key to continue".format(",".join(config)))
     ser = serial.Serial("/dev/ttyUSB0",9600)
-    for i in range(N):
+    for i in range(SAMPLES_PER_POSITION):
         chars = ser.readline()
         data = chars.strip()
-        o.write(data+"\n")
-        o.flush()
+        print data
+        
 
-o.close()
-print("DONE")
+
     
 
 
